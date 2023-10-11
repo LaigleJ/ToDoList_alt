9d4e2f6cc1d56dd1ec78f75ddf689597b5183663
Jeremy Laigle <you@example.com>
2023-10-11 14:56:04 +0200

    add modification server

--- server.js
+++ server.js
@@ -1,33 +1,33 @@
-import express from 'express';
-import sequelize from './util/database.js';
-import routerUsers from './routes/users_routes.js';
-import routerTasks from './routes/tasks_routes.js';
-
+import express from "express";
+import sequelize from "./util/database.js";
+import routerUsers from "./routes/users_routes.js";
+import routerTasks from "./routes/tasks_routes.js";
+import routerCategory from "./routes/category_routes.js";
+import routerStatus from "./routes/status_routes.js";
+import routerPriority from "/routes/priority_routes.js";
 
 async function main() {
+  const app = express();
+  const port = process.env.PORT;
 
+  app.use(express.json());
 
-const app = express();
-const port = 3000;
+  app.get("/", (req, res) => {
+    res.send("Hello World 2");
+  });
 
-app.use(express.json());
+  app.use("/", routerTasks);
+  app.use("/", routerUsers);
+  app.use("/", routerCategory);
+  app.use("/", routerStatus);
+  app.use("/", routerPriority);
 
-app.get('/', (req, res) => {
-res.send('Hello World 2')
-});
-
-app.use('/', routerTasks);
-app.use('/', routerUsers);
-
-try {
-    await sequelize.sync({force: true})
-    app.listen(port)
-    console.log("🚀 ~ file: server.js:25 ~ main ~ port:", port)
-} catch (error) {
-    console.log("🚀 ~ file: server.js:26 ~ main ~ error:", error)
+  try {
+    await sequelize.sync({ force: true });
+    app.listen(port);
+    console.log("🚀 ~ file: server.js:25 ~ main ~ port:", port);
+  } catch (error) {
+    console.log("🚀 ~ file: server.js:26 ~ main ~ error:", error);
+  }
 }
-
-
-
-}
-main()
+main();
