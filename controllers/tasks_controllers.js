32b553d0c630a249de5f9c5af733dfd618273704
Jeremy Laigle <you@example.com>
2023-10-11 15:27:28 +0200

    add modification routes Tasks and controllers Tasks

--- controllers/tasks_controllers.js
+++ controllers/tasks_controllers.js
@@ -1,45 +1,81 @@
-import {Tasks} from '../models/todolistModels.js';
+import { Tasks } from "../models/todolistModels.js";
 
 const getAllTasks = (req, res) => {
-    Tasks.findAll()
-        .then((tasks) => {
-            res.json({Tasks: tasks})
-        })
-        .catch((error) => {
-            console.log(error)
-        })
+  Tasks.findAll()
+    .then((tasks) => {
+      res.json({ Tasks: tasks });
+    })
+    .catch((error) => {
+      console.log(error);
+    });
 };
 
 const createTasks = (req, res) => {
-    Book.create({name: req.body.name, description: req.body.description, category: req.body.category, status: req.body.status, property: req.body.property, deadline: req.body.deadline, ids_users: req.body.ids_users})
-        .then((result) => {
-            res.json({"message": "Tasks created", "resultat": result})
-        })
-        .catch((error) => {
-            console.log(error)
-        })
+  const tasks = {
+    name: req.body.name,
+    description: req.body.description,
+    id_Category: req.body.category,
+    id_Status: req.body.status,
+    id_Property: req.body.property,
+    deadline: req.body.deadline,
+    author: req.body.author,
+    ids_users: req.body.ids_users,
+  };
+  Tasks.create(tasks)
+    .then((result) => {
+      res.json({ message: "Tasks created", resultat: result });
+    })
+    .catch((error) => {
+      console.log(error);
+    });
 };
 
 const getTask = (req, res) => {
-    const id_tasks = req.params.id
-    Book.findAll({
-        where: {
-            id: id_tasks
-        }
+  const id_tasks = req.params.id;
+  Book.findAll({
+    where: {
+      id: id_tasks,
+    },
+  })
+    .then((tasks) => {
+      if (!tasks) {
+        res.statut(404).json({ message: "Task not found" });
+      }
+      res.json({ Tasks: tasks });
     })
-        .then((tasks) => {
-            if (!tasks) {
-                res.statut(404).json({"message": "Task not found"})
-            }
-            res.json({Tasks: tasks})
-        })
-        .catch((error) => {
-            console.log(error)
-        })
+    .catch((error) => {
+      console.log(error);
+    });
 };
 
-export {
-    getAllTasks,
-    createTasks,
-    getTask
-}
+const updateTasks = async (req, res) => {
+    const name = req.body.name;
+    const isName = await Tasks.findAll({
+        where: {
+            name: name
+        }
+    });
+    if (isName) {
+        const tasks = {
+            name: req.body.name,
+            description: req.body.description,
+            id_Category: req.body.category,
+            id_Status: req.body.status,
+            id_Property: req.body.property,
+            deadline: req.body.deadline,
+            author: req.body.author,
+            ids_users: req.body.ids_users,
+          };
+          Tasks.update(tasks)
+            .then((result) => {
+              res.json({ message: "Tasks updated", resultat: result });
+            })
+            .catch((error) => {
+              console.log(error);
+            });
+    } else {
+        res.json({ message: "Please verify your information!" });
+      }
+};
+
+export { getAllTasks, createTasks, getTask, updateTasks };
--- routes/tasks_routes.js
+++ routes/tasks_routes.js
@@ -1,10 +1,11 @@
 import express from 'express';
-import { getAllTasks, createTasks, getTask } from '../controllers/tasks_controllers.js';
+import { getAllTasks, createTasks, getTask, updateTasks } from '../controllers/tasks_controllers.js';
 
 const routerTasks = express.Router();
 
 routerTasks.get('/tasks', getAllTasks);
 routerTasks.post('/tasks', createTasks);
-routerTasks.get('/tasks/:id', getTask)
+routerTasks.get('/tasks/:id', getTask);
+routerTasks.put('/tasks/:id', updateTasks);
 
 export default routerTasks;
