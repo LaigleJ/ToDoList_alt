cd1eb37fddcc6e3f284d89c1ee3610fc3c14c66e
Jeremy Laigle <you@example.com>
2023-10-11 16:25:10 +0200

    add modification routes users et controllers users

--- controllers/users_controllers.js
+++ controllers/users_controllers.js
@@ -1,26 +1,49 @@
-import {Users} from '../models/todolistModels.js';
+import { Users } from "../models/todolistModels.js";
+import bcrypt from "bcrypt";
 
 const getAllUsers = (req, res) => {
-    Users.findAll()
-        .then((users) => {
-            res.json({Users: users})
-        })
-        .catch((error) => {
-            console.log(error)
-        })
+  Users.findAll()
+    .then((users) => {
+      res.json({ Users: users });
+    })
+    .catch((error) => {
+      console.log(error);
+    });
 };
 
-const createUsers = (req, res) => {
-    Users.create({nickname: req.body.nickname, email: req.body.email})
+const createUsers = async (req, res) => {
+  let data = req.body;
+
+  const mail = data.email;
+  const isUser = await Users.findAll({
+    where: {
+      email: mail,
+    },
+  });
+
+  if (!isUser) {
+    const nickName = data.nickname;
+    const eMail = data.email;
+    const passWord = data.password;
+
+    if (nickName && eMail && passWord) {
+      const salt = await bcrypt.genSalt(20);
+      const hashedPassword = await bcrypt.hash(passWord, salt);
+
+      const user = {
+        nickname: nickName,
+        email: eMail,
+        password: hashedPassword,
+      };
+      Users.create(user)
         .then((result) => {
-            res.json({"message": "user created", "resultat": result})
+          res.json({ message: "user created", resultat: result });
         })
         .catch((error) => {
-            console.log(error)
-        })
+          console.log(error);
+        });
+    }
+  }
 };
 
-export {
-    getAllUsers,
-    createUsers
-}
+export { getAllUsers, createUsers };
