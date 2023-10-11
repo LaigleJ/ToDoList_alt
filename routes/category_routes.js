c682bcb31d29b319601e373765c6504127eb72ab
Jeremy Laigle <you@example.com>
2023-10-11 16:50:31 +0200

    add category_routes

---
+++ routes/category_routes.js
@@ -0,0 +1,9 @@
+import express from 'express';
+import {getAllCategory, createCategory} from ('../controllers/category_controllers.js');
+
+const routerCategory = express.Router();
+
+routerCategory.get('/category', getAllCategory);
+routerCategory.post('/category', createCategory);
+
+export default routerCategory;
