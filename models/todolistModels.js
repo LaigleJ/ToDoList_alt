6e08b9eb05466e62ab8254fdaf1e71da9fbc21c2
Jeremy Laigle <you@example.com>
2023-10-13 12:04:12 +0200

    wip jointure

--- models/todolistModels.js
+++ models/todolistModels.js
@@ -1,104 +1,137 @@
 import Sequelize from "sequelize";
-import database from '../util/database.js';
+import database from "../util/database.js";
 
-const Tasks = database.define('tasks', {
-    id: {
-        type: Sequelize.INTEGER,
-        autoIncrement: true,
-        allowNull: false,
-        primaryKey: true
-    },
-    name: {
-        type: Sequelize.STRING(80),
-        allowNull: false
-    },
-    description: {
-        type: Sequelize.STRING(450)
-    },
-    id_Category: {
-        type: Sequelize.INTEGER
-    },
-    id_Status: {
-        type: Sequelize.INTEGER,
-    },
-    id_Priority: {
-        type: Sequelize.INTEGER
-    },
-    deadline: {
-        type: Sequelize.STRING(25)
-    },
-    author: {
-        type: Sequelize.STRING(25)
-    },
-    ids_Users: {
-        type: Sequelize.ARRAY(Sequelize.INTEGER)
-    }
+const Users = database.define("users", {
+  id: {
+    type: Sequelize.INTEGER,
+    autoIncrement: true,
+    allowNull: false,
+    primaryKey: true,
+  },
+  nickname: {
+    type: Sequelize.STRING(15),
+    allowNull: false,
+  },
+  email: {
+    type: Sequelize.STRING(20),
+    allowNull: false,
+  },
+  password: {
+    type: Sequelize.STRING(100),
+    allowNull: false,
+  },
 });
 
-const Users = database.define('users', {
-    id: {
-        type: Sequelize.INTEGER,
-        autoIncrement: true,
-        allowNull: false,
-        primaryKey: true
-    },
-    nickname: {
-        type: Sequelize.STRING(15),
-        allowNull: false
-    },
-    email: {
-        type: Sequelize.STRING(20),
-        allowNull: false
-    },
-    password: {
-        type: Sequelize.STRING(100),
-        allowNull: false
-    }
+const Category = database.define("category", {
+  id: {
+    type: Sequelize.INTEGER,
+    autoIncrement: true,
+    allowNull: false,
+    primaryKey: true,
+  },
+  name: {
+    type: Sequelize.STRING(50),
+    allowNull: false,
+  },
 });
 
-const Category = database.define('category', {
-    id: {
-        type: Sequelize.INTEGER,
-        autoIncrement: true,
-        allowNull: false,
-        primaryKey: true
-    },
-    name: {
-        type: Sequelize.STRING(50),
-        allowNull: false
-    }
+const Status = database.define("status", {
+  id: {
+    type: Sequelize.INTEGER,
+    autoIncrement: true,
+    allowNull: false,
+    primaryKey: true,
+  },
+  name: {
+    type: Sequelize.STRING(50),
+    allowNull: false,
+  },
 });
 
-const Status = database.define('status', {
-    id: {
-        type: Sequelize.INTEGER,
-        autoIncrement: true,
-        allowNull: false,
-        primaryKey: true
-    },
-    name: {
-        type: Sequelize.STRING(50),
-        allowNull: false
-    }
+const Priority = database.define("priority", {
+  id: {
+    type: Sequelize.INTEGER,
+    autoIncrement: true,
+    allowNull: false,
+    primaryKey: true,
+  },
+  name: {
+    type: Sequelize.STRING(50),
+    allowNull: false,
+  },
 });
 
-const Priority = database.define('priority', {
-    id: {
-        type: Sequelize.INTEGER,
-        autoIncrement: true,
-        allowNull: false,
-        primaryKey: true
+const Tasks = database.define("tasks", {
+  id: {
+    type: Sequelize.INTEGER,
+    autoIncrement: true,
+    allowNull: false,
+    primaryKey: true,
+  },
+  name: {
+    type: Sequelize.STRING(80),
+    allowNull: false,
+  },
+  description: {
+    type: Sequelize.STRING(450),
+  },
+  id_Category: {
+    type: Sequelize.INTEGER,
+    references: {
+      model: Category,
+      key: "id",
     },
-    name: {
-        type: Sequelize.STRING(50),
-        allowNull: false
-    }
+  },
+  id_Status: {
+    type: Sequelize.INTEGER,
+    references: {
+      model: Status,
+      key: "id",
+    },
+  },
+  id_Priority: {
+    type: Sequelize.INTEGER,
+    references: {
+      model: Priority,
+      key: "id",
+    },
+  },
+  deadline: {
+    type: Sequelize.STRING(25),
+  },
+  author: {
+    type: Sequelize.STRING(20),
+  },
+  ids_Users: {
+    type: Sequelize.ARRAY(Sequelize.INTEGER),
+    references: {
+      model: Users,
+      key: "id",
+    },
+  },
 });
 
-export {
-    Users,
-    Tasks,
-    Category,
-    Status,
-    Priority
-};
+const UsersTasks = Sequelize.define('UsersTasks', {
+    UsersId: {
+        type: Sequelize.INTEGER,
+        references: {
+            model: Users,
+            key: 'id'
+        }
+    },
+    TasksId: {
+        type: Sequelize.INTEGER,
+        references: {
+            model: Tasks,
+            key: 'id'
+        }
+    }
+})
+
+Category.belongsToMany(Tasks, { throught: CategoryTasks });
+Status.belongsToMany(Tasks, { throught: StatusTasks });
+Priority.belongsToMany(Tasks, { throught: PriorityTasks });
+Users.belongsToMany(Tasks, { throught: UsersTasks });
+Category.belongsToMany(Users, { throught: UsersTasks });
+
+export { Users, Tasks, Category, Status, Priority };
